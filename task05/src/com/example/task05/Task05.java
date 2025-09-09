fefced7d9be97c7f88c6c27e773315ff36989c30
Dmitry <poddv2005@gmail.com>
2025-09-09 18:20:45 +0500

    pri-201

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -4,18 +4,20 @@ public class Task05 {
 
     public static String solution(int x) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        String numberString = Integer.toString(x);
+        char[] charArray = numberString.toCharArray();
+        int[] intArray = new int[charArray.length];
+        for (int i = 0; i < charArray.length; i++)
+            intArray[i] = Character.getNumericValue(charArray[i]);
+        for (int el : intArray) {
+            if (el % 2 == 1)
+                return "FALSE";
+        }
+        return "TRUE";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         String result = solution(1234);
         System.out.println(result);
-        */
     }
-
 }
--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -5,17 +5,15 @@ public class Task06 {
     public static int solution(int x, int y) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int z = x+y;
+        String numberString = Integer.toString(Math.abs(z));
+        char[] charArray = numberString.toCharArray();
+        return charArray.length;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         int result = solution(12, 34);
         System.out.println(result);
-        */
     }
 
 }
